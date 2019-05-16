3eebcc25fcb9628eaf26b2aa642669a985d387b7
sanjeevbk <sanjeevbk789@gmail.com>
2019-05-17 00:54:22 +0545

    Third and Fourth week commit

---
+++ week34/DroidCafe/app/src/main/java/com/example/droidcafe/OrderActivity.java
@@ -0,0 +1,22 @@
+package com.example.droidcafe;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class OrderActivity extends AppCompatActivity {
+    private TextView tvItem;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order);
+
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+
+        tvItem = findViewById(R.id.tvItem);
+        tvItem.setText(message);
+    }
+}
